c39378b8af63ed78a4fa5389e328a7744c7786de
Konul Abbasova <konul@Konuls-MacBook-Air.local>
2026-04-23 21:39:42 +0400

    Update EngineeringBay task processing logic and improve output format

--- src/main/java/processors/EngineeringBay.java
+++ src/main/java/processors/EngineeringBay.java
@@ -1,13 +1,17 @@
 package main.java.processors;
 import main.java.tasks.ColonyTask;
+import main.java.tasks.EngineeringTask;
+
 public class EngineeringBay implements IProcessors {
     @Override
     public String getProcessorName(){
-        return "ENGINEERING BAY";
+        return "Engineering Bay";
     }
     @Override
     public String processTask(ColonyTask task){
-        return "Engineering Bay processed: "+ task.getName();
+        EngineeringTask t = (EngineeringTask) task;
+        return "Engineering Bay repaired [" +t.getName()+ "]" +" | Parts used: "+t.getRequiredParts()+" | Power used: "
+                +t.getPowerRequired()+" | Time: "+t.getTimetoFix()+"s";
     }
     @Override
     public boolean canProcess(ColonyTask task){
