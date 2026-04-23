204c0f753adcf5cfd45404602c773cc6c827188e
Nuray Namazova <nnamazova20412@ada.edu.az>
2026-04-23 22:00:57 +0400

    Implemented Hydroponic and MedicalWard

--- src/main/java/processors/MedicalWard.java
+++ src/main/java/processors/MedicalWard.java
@@ -1,4 +1,30 @@
 package main.java.processors;
 
-public class MedicalWard {
+import main.java.tasks.ColonyTask;
+import main.java.tasks.LifeSupportTask;
+
+public class MedicalWard implements IProcessors {
+    @Override
+    public String getProcessorName()
+    {
+        return "Medical Ward";
+    }
+
+    @Override
+    public String processTask(ColonyTask task)
+    {
+        LifeSupportTask t = (LifeSupportTask) task;
+
+        return "Medical Ward resolved [" + t.getName() + "]"
+                + " | O2 used: " + t.getOxygen()
+                + " | Water used: " + t.getWaterAmount()
+                + " | Parts used: " + t.getRequiredParts()
+                + " | Time: " + t.getTimetoFix() + "s";
+    }
+
+    @Override
+    public boolean canProcess(ColonyTask task)
+    {
+        return "LIFE_SUPPORT".equals(task.getProcessorType());
+    }
 }
